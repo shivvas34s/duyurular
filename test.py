c3ba4cd7e7b039fba059b2ba51dd510971ad2cd3
shivvas34s <168650680+shivvas34s@users.noreply.github.com>
2024-05-01 19:14:55 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,7 @@
 import subprocess
-import platform
 
-def open_command_prompt():
-    # İşletim sistemini kontrol et
-    operating_system = platform.system()
+def start_chrome():
+    subprocess.Popen(["start", "chrome"], shell=True)
 
-    # İşletim sistemine göre komut istemcisini aç
-    if operating_system == 'Windows':
-        subprocess.call('cmd', shell=True)
-    elif operating_system == 'Linux':
-        subprocess.call('gnome-terminal', shell=True)
-    elif operating_system == 'Darwin':  # macOS
-        subprocess.call('Terminal', shell=True)
-    else:
-        print("İşletim sistemi desteklenmiyor.")
-
-# Komut istemcisini aç
-open_command_prompt()
+# Chrome'u başlat
+start_chrome()
